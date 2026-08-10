69e9ea69de5d6c81015a1ad692fbaf754b55aae6
Ole H. <91332299+olehrstm@users.noreply.github.com>
2026-08-10 13:05:42 +0200

    feat(payload): add channel-based payload blocking (#12)

---
+++ src/main/java/de/ole101/mctrafficcontrol/mixin/ClientboundCustomPayloadPacketMixin.java
@@ -0,0 +1,32 @@
+package de.ole101.mctrafficcontrol.mixin;
+
+import net.minecraft.network.protocol.common.ClientCommonPacketListener;
+import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static de.ole101.mctrafficcontrol.McTrafficControl.configuration;
+
+@Mixin(ClientboundCustomPayloadPacket.class)
+public class ClientboundCustomPayloadPacketMixin {
+
+    @Final
+    @Shadow
+    private CustomPacketPayload payload;
+
+    @Inject(
+            method = "handle(Lnet/minecraft/network/protocol/common/ClientCommonPacketListener;)V",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void mtc$blockIncomingPayload(ClientCommonPacketListener listener, CallbackInfo ci) {
+        if (configuration.payload().channelBlocking.isIncomingEnabled() && configuration.payload().channelBlocking.getIncomingChannels().contains(payload.type().id().toString())) {
+            ci.cancel();
+        }
+    }
+}
